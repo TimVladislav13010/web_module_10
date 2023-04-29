77d6dac7c3b7a7740bbc275d10a4f3bef8a68480
Vladislav <109065019+TimVladislav13010@users.noreply.github.com>
2023-04-29 17:13:34 +0300

    env

--- quotes/db_mongo.py
+++ quotes/db_mongo.py
@@ -1,14 +1,15 @@
 import configparser
 import pathlib
 
+from quotes.quotes.settings import env
 
 file_config = pathlib.Path(__file__).parent.joinpath('config.ini')
 config = configparser.ConfigParser()
 config.read(file_config)
 
-username = config.get('DB', 'user')
-password = config.get('DB', 'password')
-db_name = config.get('DB', 'db_name')
-domain = config.get('DB', 'domain')
+username = env("MONGO_DB_USER")
+password = env("MONGO_DB_PASSWORD")
+db_name = env("MONGO_DB_NAME")
+domain = env("MONGO_DB_DOMAIN")
 
 url = f'mongodb+srv://{username}:{password}@{domain}/{db_name}?retryWrites=true&w=majority'
